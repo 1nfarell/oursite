98e0e19be9dd274404277b66dfbe60b2e990f8dd
fdaniil <frolov.dan.mail@gmail.com>
2023-03-04 16:40:37 +0300

    feat: bread crumbs

--- post.php
+++ post.php
@@ -6,6 +6,7 @@ include 'vendor/outputPost.php';
 
 <!doctype html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,6 +16,7 @@ include 'vendor/outputPost.php';
     <link rel="stylesheet" href="/assets/css/itc-slider.min.css">
     <link rel="stylesheet" href="/assets/css/index.css">
     <link rel="stylesheet" href="/assets/css/blog.css">
+    <link rel="stylesheet" href="/assets/css/breadcrumbs.css">
     <meta name="application-name" content="">
 
     <meta property="og:type" content="website">
@@ -25,123 +27,126 @@ include 'vendor/outputPost.php';
     <meta property="og:site_name" content="Web Studio MNDP">
     <meta property="og:image" content="/images/banner.jpg">
     <meta name="robots" content="index,follow">
-   
+
     <meta name="description" content="">
-    
-  
+
+
     <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
-    <link rel="manifest" href="/images/site.webmanifest">     
+    <link rel="manifest" href="/images/site.webmanifest">
 </head>
-<body>
-<div class="container" id="ancor">
 
- <!-- хедер  -->
- <header>
-    <div id="header" class="header-wrapper">
-        <div class="header">  
-            <div class="header-left"> 
-                <a id="hide-logo" class="logo" href="#ancor">                        
-                    <img class="logo-icon" src="/images/logo.png" alt="Web studio MndP">
-                </a>               
-                <div class="site-description">
-                    <span>СТУДИЯ ВЕБ-ДИЗАЙНА MNDP</span>  
-                </div> 
-            </div>           
-            <div class="header-ref-hide">                           
-                <div class="dropdown dropdown-high">                                
-                    РАЗРАБОТКА САЙТОВ
-                    <img class="icon-dropdown" src="/images/plus.png" alt="меню">
-                    <div class="dropdown-content">                                
-                        <a class="header-ref" href="/#stage">ЭТАПЫ РАБОТЫ</a>
-                        <a class="header-ref" href="/#price">УСЛУГИ</a>
-                        <a class="header-ref" href="/#ask">ОТВЕТЫ НА ВОПРОСЫ</a>
-                        <a class="header-ref" href="/#portfolio">ПОРТФОЛИО</a>
-                        <a class="header-ref" href="/#about">О НАС</a>  
+<body>
+    <div class="container" id="ancor">
+
+        <!-- хедер  -->
+        <header>
+            <div id="header" class="header-wrapper">
+                <div class="header">
+                    <div class="header-left">
+                        <a id="hide-logo" class="logo" href="#ancor">
+                            <img class="logo-icon" src="/images/logo.png" alt="Web studio MndP">
+                        </a>
+                        <div class="site-description">
+                            <span>СТУДИЯ ВЕБ-ДИЗАЙНА MNDP</span>
+                        </div>
+                    </div>
+                    <div class="header-ref-hide">
+                        <div class="dropdown dropdown-high">
+                            РАЗРАБОТКА САЙТОВ
+                            <img class="icon-dropdown" src="/images/plus.png" alt="меню">
+                            <div class="dropdown-content">
+                                <a class="header-ref" href="/#stage">ЭТАПЫ РАБОТЫ</a>
+                                <a class="header-ref" href="/#price">УСЛУГИ</a>
+                                <a class="header-ref" href="/#ask">ОТВЕТЫ НА ВОПРОСЫ</a>
+                                <a class="header-ref" href="/#portfolio">ПОРТФОЛИО</a>
+                                <a class="header-ref" href="/#about">О НАС</a>
+                            </div>
+                        </div>
+                        <div class="dropdown dropdown-high">
+                            РАБОТА С ТЕКСТОМ
+                            <img class="icon-dropdown" src="/images/plus.png" alt="меню">
+                            <div class="dropdown-content">
+                                <a class="header-ref" href="/copywriting">КОПИРАЙТ ТЕКСТА</a>
+                                <a class="header-ref" href="/rewriting">РЕРАЙТ ТЕКСТА</a>
+                            </div>
+                        </div>
+                        <a class="header-ref" href="/blog">БЛОГ</a>
+
+                    </div>
+                    <div class="menu">
+                        <div class="dropdown dropdown-show">
+                            <img class="icon-message" src="/images/menu.png" alt="меню">
+                            <div class="dropdown-content">
+                                <a class="header-ref" href="/#stage">ЭТАПЫ РАБОТЫ</a>
+                                <a class="header-ref" href="/#price">УСЛУГИ</a>
+                                <a class="header-ref" href="/#ask">ОТВЕТЫ НА ВОПРОСЫ</a>
+                                <a class="header-ref" href="/#portfolio">ПОРТФОЛИО</a>
+                                <a class="header-ref" href="/#about">О НАС</a>
+                                <a class="header-ref" href="/copywriting">КОПИРАЙТ ТЕКСТА</a>
+                                <a class="header-ref" href="/rewriting">РЕРАЙТ ТЕКСТА</a>
+                                <a class="header-ref" href="/blog">БЛОГ</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="dropdown dropdown-high">                                
-                    РАБОТА С ТЕКСТОМ
-                    <img class="icon-dropdown" src="/images/plus.png" alt="меню">
-                    <div class="dropdown-content">                                
-                        <a class="header-ref" href="/copywriting">КОПИРАЙТ ТЕКСТА</a>
-                        <a class="header-ref" href="/rewriting">РЕРАЙТ ТЕКСТА</a>                                   
-                    </div>
-                </div>
-                <a class="header-ref" href="/blog">БЛОГ</a> 
-                            
             </div>
-            <div class="menu">
-                <div class="dropdown dropdown-show">
-                    <img class="icon-message" src="/images/menu.png" alt="меню">
-                    <div class="dropdown-content">
-                        <a class="header-ref" href="/#stage">ЭТАПЫ РАБОТЫ</a>
-                        <a class="header-ref" href="/#price">УСЛУГИ</a>
-                        <a class="header-ref" href="/#ask">ОТВЕТЫ НА ВОПРОСЫ</a>
-                        <a class="header-ref" href="/#portfolio">ПОРТФОЛИО</a>
-                        <a class="header-ref" href="/#about">О НАС</a>  
-                        <a class="header-ref" href="/copywriting">КОПИРАЙТ ТЕКСТА</a>
-                        <a class="header-ref" href="/rewriting">РЕРАЙТ ТЕКСТА</a>
-                        <a class="header-ref" href="/blog">БЛОГ</a>   
+        </header>
+        <!-- main -->
+        <div class="container-block-1">
+            <div class="wrapper-block">
+                <div class="main main-color">
+                    <div class="main-post">
+
+                        <?php generationOutput($shouldCountView); ?>
                     </div>
                 </div>
-            </div>                  
+            </div>
+
+            <div class="wrapper-block">
+                <div class="pricesite-title">
+                    <h2>Читайте и другие статьи в блоге MNDP</h2>
+                </div>
+                <div class="main">
+                    <div id="main-center" class="main-center">
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- фон и курсор -->
+            <div class="lines">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+            </div>
         </div>
-    </div>  
-</header>   
-<!-- main -->
-<div class="container-block-1">
-    <div class="wrapper-block">
-        <div class="main main-color">        
-            <div class="main-post">
-                <?php generationOutput($shouldCountView); ?>                              
-            </div>    
-        </div>
-    </div> 
-    
-    <div class="wrapper-block">
-        <div class="pricesite-title">
-            <h2>Читайте и другие статьи в блоге MNDP</h2>
-        </div>
-        <div class="main">        
-            <div id="main-center" class="main-center">
-                
-            </div>                    
-        </div>
+        <!-- футтер -->
+        <footer>
+            <div class="container-background-block-1 offer-border container-radius-top">
+                <div class="container-block-1 ">
+
+                    <div class="footer-row">
+                        <div class="footer-row-column">
+                            <span>По вопросам сотрудничества писать на почту contact@mndp-studio.ru</span>
+                        </div>
+                        <div>
+                            2022 - 2023 © Made with love by MndP.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
-                 
-    <!-- фон и курсор -->
-    <div class="lines">
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>                    
-    </div>    
-</div>
-<!-- футтер -->
-    <footer>
-        <div class="container-background-block-1 offer-border container-radius-top"> 
-            <div class="container-block-1 "> 
-                
-                <div class="footer-row">               
-                    <div class="footer-row-column">
-                        <span>По вопросам сотрудничества писать на почту contact@mndp-studio.ru</span>
-                    </div>                    
-                    <div>
-                        2022 - 2023 © Made with love by MndP.                           
-                    </div>   
-                </div>                           
-            </div> 
-        </div>   
-    </footer>  
-</div>
-<!-- jquery -->
-<script src="/assets/js/jquery-3.6.1.min.js"></script>
-<!-- все скрипты -->
-<script src="/assets/js/script.js" defer></script>
-<script src="/assets/js/outlikepost.js"></script>
+    <!-- jquery -->
+    <script src="/assets/js/jquery-3.6.1.min.js"></script>
+    <!-- все скрипты -->
+    <script src="/assets/js/script.js" defer></script>
+    <script src="/assets/js/outlikepost.js"></script>
 
 
 </body>
+
 </html>
--- vendor/outputPost.php
+++ vendor/outputPost.php
@@ -1,7 +1,7 @@
 <?php
 $shouldCountView = false;
-if(!isset($_COOKIE['suid_' . md5($_GET['id'])])) {
-    setcookie('suid_' . md5($_GET['id']), date('d,m,Y H:i:s', time()),time()+(24*3600));
+if (!isset($_COOKIE['suid_' . md5($_GET['id'])])) {
+    setcookie('suid_' . md5($_GET['id']), date('d,m,Y H:i:s', time()), time() + (24 * 3600));
     $shouldCountView = true;
 }
 require_once 'StaticConnection.php';
@@ -21,10 +21,10 @@ function generationOutput($shouldCountView)
     //   : $_SERVER['REMOTE_ADDR']);
 
     //$sth = $db->prepare("SELECT ")
-    
+
     $db = StaticConnection::getConnection();
 
-    if($shouldCountView) {
+    if ($shouldCountView) {
         $state = $db->prepare("UPDATE articles SET views = views + 1 WHERE id = :id");
         $state->bindValue('id', $id);
         $state->execute();
@@ -36,87 +36,89 @@ function generationOutput($shouldCountView)
     JOIN users ON articles.id_username = users.id
     WHERE articles.id = '$id'
     GROUP BY articles.id");
-    
+
     $sth->execute();
 
-    if ($sth->rowCount() > 0){
-        while($article = $sth->fetch(PDO::FETCH_ASSOC)){  
-            ?> 
+    if ($sth->rowCount() > 0) {
+        while ($article = $sth->fetch(PDO::FETCH_ASSOC)) {
+?>
             <div class="post">
-                <img class="post-image" <?= $id_article = $article['id']; 
-                                                $sth = $db->prepare("SELECT recipe_picture_boolean, images.id_article, images.image_name, images.image_tmp
+                <img class="post-image" <?= $id_article = $article['id'];
+                                        $sth = $db->prepare("SELECT recipe_picture_boolean, images.id_article, images.image_name, images.image_tmp
                                                 FROM images            
-                                                WHERE images.id_article = $id_article AND recipe_picture_boolean = 1"); 
-                                                $sth->execute();
-                                                $image = $sth->fetch(PDO::FETCH_ASSOC); ?> src="data:image/jpeg;base64, <?= base64_encode($image['image_tmp']) ?>">
-                <h1 class="post-title" ><?= $article['title'] ?></a></h1>  
-                          
-            </div>    
+                                                WHERE images.id_article = $id_article AND recipe_picture_boolean = 1");
+                                        $sth->execute();
+                                        $image = $sth->fetch(PDO::FETCH_ASSOC); ?> src="data:image/jpeg;base64, <?= base64_encode($image['image_tmp']) ?>">
+                <h1 class="post-title"><?= $article['title'] ?></a></h1>
 
-            
+            </div>
+
+            <ul class="breadcrumb">
+                <li><a href="/">Главная</a></li>
+                <li><a href="/blog.php">Все посты</a></li>
+                <li><?= $article['title'] ?></li>
+            </ul>
             <div class="post-general">
-                <div class="post-cooking-process">  
+
+                <div class="post-cooking-process">
                     <div class="post-description">
                         <p class="post-text-description"><?= $article['description'] ?></p>
-                    </div>                    
-                        <?php $id_article = $article['id'];                                  
-                                $sth = $db->prepare("SELECT text, recept_text.id_article, recept_text.numeration, recept_text.header_text, images.image_name, images.image_tmp 
+                    </div>
+                    <?php $id_article = $article['id'];
+                    $sth = $db->prepare("SELECT text, recept_text.id_article, recept_text.numeration, recept_text.header_text, images.image_name, images.image_tmp 
                                                         FROM recept_text  
                                                         JOIN images ON recept_text.numeration = images.recipe_picture_boolean 
                                                         WHERE recept_text.id_article = $id_article AND images.id_article = $id_article 
                                                         ORDER BY numeration;");
-                                $sth->execute();
+                    $sth->execute();
 
-                                if ($sth->rowCount() > 0){
-                                    while($value = $sth->fetch(PDO::FETCH_ASSOC)){ 
-                                        ?>  <div class="post-cooking-process-field">
-                                                <div class="post-picture">
-                                                    <img class="post-picture-image" src="data:image/jpeg;base64, <?= base64_encode($value['image_tmp']) ?>">
-                                                    <h2 class="post-text-header"> <?= $value['header_text'] ?></h2>
-                                                </div>
-                                                <div class="post-text">                                                    
-                                                    <div class="post-text-value"> <?= $value['text'] ?></div> 
-                                                </div>
-                                            </div>
-                                        <?php
-                                    } 
-                                } 
-                                ?>
-                    
-                </div> 
+                    if ($sth->rowCount() > 0) {
+                        while ($value = $sth->fetch(PDO::FETCH_ASSOC)) {
+                    ?> <div class="post-cooking-process-field">
+                                <div class="post-picture">
+                                    <img class="post-picture-image" src="data:image/jpeg;base64, <?= base64_encode($value['image_tmp']) ?>">
+                                    <h2 class="post-text-header"> <?= $value['header_text'] ?></h2>
+                                </div>
+                                <div class="post-text">
+                                    <div class="post-text-value"> <?= $value['text'] ?></div>
+                                </div>
+                            </div>
+                    <?php
+                        }
+                    }
+                    ?>
+
+                </div>
                 <div class="post__rightcontainer">
-1
                 </div>
             </div>
             <div class="info">
-                    <div class="card-autor">    
-                        <img class="card-icon-autor" src="images\icon-user.png">                    
-                        <a href="autor.php?user=<?= $article['id_user'] ?>">
-                            <p class="post-text-autor"><?= $article['full_name'] ?></p> 
-                        </a>                    
-                    </div> 
-                    
-                    <div class="post-headdescription">
-                        <div class="post-date">    
-                            <img class="post-icon-date" src="images\eye.png">
-                            <p class="post-text-views"><?= $article['views'] ?> </p>
-                        </div>
+                <div class="card-autor">
+                    <img class="card-icon-autor" src="images\icon-user.png">
+                    <a href="autor.php?user=<?= $article['id_user'] ?>">
+                        <p class="post-text-autor"><?= $article['full_name'] ?></p>
+                    </a>
+                </div>
 
-                        <div class="post-date">
-                            <img class="post-icon-date" src="images\date.png">
-                            <p class="post-date"><?= date("d.m.Y",strtotime($article['date'])) ?> </p>
-                        </div>  
+                <div class="post-headdescription">
+                    <div class="post-date">
+                        <img class="post-icon-date" src="images\eye.png">
+                        <p class="post-text-views"><?= $article['views'] ?> </p>
                     </div>
 
-                    <div class="card-id"> 
-                        <img class="card-icon-id" src="images\hashtag-sign.png">
-                        <p class="post-id-name"><?= $article['name'] ?></p>
+                    <div class="post-date">
+                        <img class="post-icon-date" src="images\date.png">
+                        <p class="post-date"><?= date("d.m.Y", strtotime($article['date'])) ?> </p>
                     </div>
                 </div>
 
-            <?php             
-        }     
-    } else echo "Ошибка..";   
+                <div class="card-id">
+                    <img class="card-icon-id" src="images\hashtag-sign.png">
+                    <p class="post-id-name"><?= $article['name'] ?></p>
+                </div>
+            </div>
+
+<?php
+        }
+    } else echo "Ошибка..";
 }
-
-
