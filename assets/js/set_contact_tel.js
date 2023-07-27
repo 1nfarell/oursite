ddfe8f34c0a95b129c269763f53a891441c6af67
1nfarell <maksbeketskii@gmail.com>
2023-07-27 19:55:04 +0300

    final3.0

--- assets/js/set_contact_tel.js
+++ assets/js/set_contact_tel.js
@@ -6,5 +6,5 @@ let contact_tel_2 = $('#contact_tel_2');
 contact_tel_2.text('8 (937) 10-6666-9');
 // telegram и whatsup: ../ 
 document.getElementById('telegram_href').href = 'tg://resolve?domain=managerOtKR'
-document.getElementById('whatsup_href').href = 'https://wa.me/79377307007?text=Добрый&#32день.&#32Какой&#32вопрос&#32Вас&#32интересует?'
+document.getElementById('whatsup_href').href = 'https://wa.me/79377307007?text=Добрый+день.+Какой+вопрос+Вас+интересует?'
 // =======================================================================
